f6055960755a8b17713dfe72a7e76f323d7fdb4d
BBwanaz <bbwanakocha@usapglobal.org>
2020-05-24 19:48:48 -0700

    routes to portfolios updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+ 
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
