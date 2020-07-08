0d05f9eb15394b1bb22c60785b0ad0c2e6c63003
BBwanaz <bbwanakocha@usapglobal.org>
2020-07-08 16:01:07 -0700

    Included topic in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Topic.create!(title: "Bwanaz")
 10.times do |item|
 
 	Portfolio.create!(
 		title: "Portfolio #{item}",
 		body:  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-		image: "https://via.placeholder.com/150"
+		image: "https://via.placeholder.com/150",
+		topic: Topic.first
 		)
 
 end
